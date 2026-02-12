45a01567e5ab459d830aff72a4825a5c7eea5fdc
Vitor <vitor41197@gmail.com>
2026-02-12 20:57:13 -0300

    Board generation and move logic working

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function Gameboard(){
         ["| |","| |","| |"]
     ]
     this.showBoard = function(){
+        console.log("this is the board right now")
         console.log(this.board[0])
         console.log(this.board[1])
         console.log(this.board[2])
@@ -30,7 +31,6 @@ newBoard = new Gameboard()
 player1.sayName()
 newBoard.showBoard()
 
-
 function playerMove(){
     switch (currentPlayer) {
         case 0:
@@ -43,75 +43,75 @@ function playerMove(){
     playerChoice = prompt('write position: a1 a2 a3 b1 b2 b3 c1 c2 c3')
     switch (true) {
         case (playerChoice == "a1" && currentPlayer == 1):
-        newBoard[0[0]] = player1.marker
+        newBoard.board[0][0] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "a1" && currentPlayer == 2):
-        newBoard[0[0]] = player2.marker
+        newBoard.board[0][0] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "a2" && currentPlayer == 1):
-        newBoard[0[1]] = player1.marker
+        newBoard.board[0][1] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "a2" && currentPlayer == 2):
-        newBoard[0[1]] = player2.marker
+        newBoard.board[0][1] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "a3" && currentPlayer == 1):
-        newBoard[0[2]] = player1.marker
+        newBoard.board[0][2] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "a3" && currentPlayer == 2):
-        newBoard[0[2]] = player2.marker
+        newBoard.board[0][2] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "b1" && currentPlayer == 1):
-        newBoard[1[0]] = player1.marker
+        newBoard.board[1][0] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "b1" && currentPlayer == 2):
-        newBoard[1[0]] = player2.marker
+        newBoard.board[1][0] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "b2" && currentPlayer == 1):
-        newBoard[1[1]] = player1.marker
+        newBoard.board[1][1] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "b2" && currentPlayer == 2):
-        newBoard[1[1]] = player2.marker
+        newBoard.board[1][1] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "b3" && currentPlayer == 1):
-        newBoard[1[2]] = player1.marker
+        newBoard.board[1][2] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "b3" && currentPlayer == 2):
-        newBoard[1[2]] = player2.marker
+        newBoard.board[1][2] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "c1" && currentPlayer == 1):
-        newBoard[2[0]] = player1.marker
+        newBoard.board[2][0] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "c1" && currentPlayer == 2):
-        newBoard[2[0]] = player2.marker
+        newBoard.board[2][0] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "c2" && currentPlayer == 1):
-        newBoard[2[1]] = player1.marker
+        newBoard.board[2][1] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "c2" && currentPlayer == 2):
-        newBoard[2[1]] = player2.marker
+        newBoard.board[2][1] = player2.marker
         newBoard.showBoard()
         break
         case (playerChoice == "c3" && currentPlayer == 1):
-        newBoard[2[2]] = player1.marker
+        newBoard.board[2][2] = player1.marker
         newBoard.showBoard()
         break
         case (playerChoice == "c3" && currentPlayer == 2):
-        newBoard[2[2]] = player2.marker
+        newBoard.board[2][2] = player2.marker
         newBoard.showBoard()
         break
     }
